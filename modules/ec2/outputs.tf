753c316565a20d6a1ec291fb5ceeee6ba6b8b276
nagy <nagy.eid1@nhs.net>
2026-08-05 11:09:52 +0100

    Ignore Visual Studio workspace folder

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -12,3 +12,23 @@ output "security_group_id" {
   description = "ID of the web server security group."
   value       = aws_security_group.web.id
 }
+
+output "vpc_id" {
+  description = "ID of the VPC."
+  value       = module.network.vpc_id
+}
+
+output "public_subnet_id" {
+  description = "ID of the public subnet."
+  value       = module.network.public_subnet_id
+}
+
+output "ec2_instance_id" {
+  description = "ID of the EC2 web server."
+  value       = module.ec2.instance_id
+}
+
+output "ec2_public_ip" {
+  description = "Public IP address of the EC2 web server."
+  value       = module.ec2.public_ip
+}
